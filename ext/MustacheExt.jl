4d05dcd37c63290e2f405c9ebcadad4ca8b8757e
Christopher Rowley <cjdrwly@gmail.com>
2026-01-07 22:52:36 +0000

    Inline Mustache template view (#25)

--- ext/MustacheExt.jl
+++ ext/MustacheExt.jl
@@ -7,33 +7,21 @@ using Printf
 include(joinpath(@__DIR__, "..", "src", "templating.jl"))
 
 """
-    template_view(info)
-
-Return a template view containing the standard ntfy substitution values.
-"""
-function template_view(info)
-    view = Dict{Any,Any}()
-    function add_key!(key::AbstractString, value)
-        view[key] = value
-        view[Symbol(key)] = value
-    end
-    for key in TEMPLATE_KEYS
-        if key === :is_error
-            add_key!(String(key), template_value(key, info))
-        else
-            add_key!(String(key), () -> template_value(key, info))
-        end
-    end
-    return view
-end
-
-"""
     Ntfy.render_template(template::Mustache.MustacheTokens, info)
 
 Render a Mustache template using the standard ntfy substitution values.
 """
 function Ntfy.render_template(template::Mustache.MustacheTokens, info)
-    return Mustache.render(template, template_view(info))
+    view = Dict{String,Any}()
+    for key in TEMPLATE_KEYS
+        key_string = String(key)
+        if key === :is_error
+            view[key_string] = template_value(key, info)
+        else
+            view[key_string] = () -> template_value(key, info)
+        end
+    end
+    return Mustache.render(template, view)
 end
 
 end
