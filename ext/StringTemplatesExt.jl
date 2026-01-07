9c920f74beec1e71655ca36341a7916d227aa2fa
Christopher Rowley <cjdrwly@gmail.com>
2026-01-07 20:44:29 +0000

    Inline StringTemplates template view (#23)

--- ext/StringTemplatesExt.jl
+++ ext/StringTemplatesExt.jl
@@ -7,25 +7,16 @@ using StringTemplates
 include(joinpath(@__DIR__, "..", "src", "templating.jl"))
 
 """
-    template_view(info)
-
-Return a template view containing the standard ntfy substitution values.
-"""
-function template_view(info)
-    view = Dict{Symbol,Any}()
-    for key in TEMPLATE_KEYS
-        view[key] = template_value(key, info)
-    end
-    return view
-end
-
-"""
     Ntfy.render_template(template::StringTemplates.Template, info)
 
 Render a StringTemplates template using the standard ntfy substitution values.
 """
 function Ntfy.render_template(template::StringTemplates.Template, info)
-    return StringTemplates.render(template, template_view(info))
+    view = Dict{Symbol,Any}()
+    for key in unique(StringTemplates.props(template))
+        view[key] = template_value(key, info)
+    end
+    return StringTemplates.render(template, view)
 end
 
 end
