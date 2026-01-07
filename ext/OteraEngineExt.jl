45a8c6290225b3498747b99c7d6fb564f35202e7
Christopher Rowley <cjdrwly@gmail.com>
2026-01-07 21:17:02 +0000

    Streamline OteraEngine template rendering

--- ext/OteraEngineExt.jl
+++ ext/OteraEngineExt.jl
@@ -7,25 +7,16 @@ using Printf
 include(joinpath(@__DIR__, "..", "src", "templating.jl"))
 
 """
-    template_view(info)
-
-Return a template view containing the standard ntfy substitution values.
-"""
-function template_view(info)
-    view = Dict{Symbol,Any}()
-    for key in TEMPLATE_KEYS
-        view[key] = template_value(key, info)
-    end
-    return view
-end
-
-"""
     Ntfy.render_template(template::OteraEngine.Template, info)
 
 Render an OteraEngine template using the standard ntfy substitution values.
 """
 function Ntfy.render_template(template::OteraEngine.Template, info)
-    return template(init=template_view(info))
+    view = Dict{Symbol,Any}()
+    for key in template.args
+        view[key] = template_value(key, info)
+    end
+    return template(init=view)
 end
 
 end
